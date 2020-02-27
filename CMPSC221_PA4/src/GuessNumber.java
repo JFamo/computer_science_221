3ac3263f8ac1bc04272ae52d6c2fa5754a825c44
J. Famous <joshua.famous@gmail.com>
2020-02-26 23:45:12 -0500

    Working label

--- CMPSC221_PA4/src/GuessNumber.java
+++ CMPSC221_PA4/src/GuessNumber.java
@@ -1,25 +1,49 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class GuessNumber {
+public class GuessNumber extends JFrame {
 	
-	public static void main(String args[]) {
+	private final JLabel labelInitial;
+	private final JLabel labelProximity;
+	private final JPanel contentPane;
+	private final JTextField inputGuess;
+	private final JButton buttonGuess;
+	private final JButton buttonPlayAgain;
 		
-		// Create GUI objects
-		JFrame frame = new JFrame("Guess the Number Game");
-		JLabel labelInitial = new JLabel("I have a number between 1 and 1000. Can you guess my number?\nPlease enter your first guess.", JLabel.CENTER);
-		JLabel labelProximity = new JLabel("Too high!");
-		JTextField inputGuess = new JTextField("",4);
+	public GuessNumber() {
 		
+		// Create frame
+		super("Guess the Number Game");
+		setLayout(new FlowLayout());
+		
+		// Create and configure content pane panel
+		contentPane = new JPanel(null);
+		contentPane.setPreferredSize(new Dimension(500,400));
+		contentPane.setBackground(new Color(51,102,255));
+		
+		// Create labels
+		labelInitial = new JLabel("I have a number between 1 and 1000. Can you guess my number? Please enter your first guess.");
+		labelProximity = new JLabel("Too high!");
+		
+		// Create text field
+		inputGuess = new JTextField(4);
+		
+		// Create buttons
+		buttonGuess = new JButton("Guess");
+		buttonPlayAgain = new JButton("Play Again");
+	
 		// Set object display options
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(labelInitial, BorderLayout.CENTER);
-		frame.getContentPane().add(inputGuess, BorderLayout.CENTER);
+		contentPane.add(labelInitial);
+		contentPane.add(labelProximity);
+		contentPane.add(inputGuess);
+		contentPane.add(buttonGuess);
+		contentPane.add(buttonPlayAgain);
 		
 		// Set frame display options
-		frame.setLocationRelativeTo(null);
-		frame.pack();
-		frame.setVisible(true);
+		add(labelInitial);
+		setLocationRelativeTo(null);
+		pack();
+		setVisible(true);
 	}
 	
 }
---
+++ CMPSC221_PA4/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+		GuessNumber gui = new GuessNumber();
+		
+	}
+	
+}
